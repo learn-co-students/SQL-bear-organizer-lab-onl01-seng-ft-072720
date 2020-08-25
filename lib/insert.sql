090197b29ece05ca6b339fc42e9381d2cbeb2c7e
Aaron Brady <aleebrady@gmail.com>
2020-08-25 19:05:50 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 10, 'Male', 'Brown', 'Yogi-Like', 0), 
+('Rowdy', 10, 'Male', 'Brown', 'Yogi-Like', 0),
+('Tabitha', 10, 'Male', 'Brown', 'Yogi-Like', 0),
+('Sergeant Brown', 10, 'Male', 'Brown', 'Yogi-Like', 0),
+('Melissa', 10, 'Male', 'Brown', 'Yogi-Like', 0),
+('Grinch', 10, 'Male', 'Brown', 'Yogi-Like', 0),
+('Wendy', 10, 'Male', 'Brown', 'Yogi-Like', 0),
+(NULL, 10, 'Male', 'Brown', 'Aggressive', 0);
